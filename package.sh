ac3b92a03613ba8c53232ba76038c5ae933cb93d
ryan <csf0711@gmail.com>
2017-02-20 14:03:26 +0800

    update

--- package.sh
+++ package.sh
@@ -18,30 +18,48 @@ find $1/. -name  '*.patch' | while read filename; do mv -v "${filename}" "`echo 
 if [ "$2" == "bmc" ]; then
         find $1/. -name  '*.bmc' | while read filename; do mv -v "${filename}" "`echo "${filename}" | sed -e 's/.bmc//'`"; done
 	find $1/. -name  '*.cam' | while read filename; do rm -f "${filename}"; done
+	rm -rf $1/board/aspeed/ast1*
+	rm -f $1/arch/arm/mach-aspeed/*cam*.c
+	rm -f $1/arch/arm/mach-aspeed/include/mach/*cam*.h
+	rm -rf $1/board/aspeed/ast-cam
 	rm -rf $1/arch/m68k/cpu/aspeed
 	rm -f $1/drivers/spi/fmc_spi.c
 	rm -f $1/arch/arm/cpu/ast-common/spl.c
 	rm -f $1/arch/arm/include/asm/arch-aspee/spl.h
+	rm -f $1/include/configs/ast-cam.h
+	rm -f $1/configs/ast1220_defconfig
+	rm -f $1/drivers/mmc/ast_sdhci.c
+	rm -f $1/drivers/usb/gadget/ast_udc.c
+	rm -f $1/drivers/memory/ast-cam-sdmc.c
 fi
 
 #CAM
 if [ "$2" == "cam" ]; then
 	find $1/. -name  '*.cam' | while read filename; do mv -v "${filename}" "`echo "${filename}" | sed -e 's/.cam//'`"; done
 	find $1/. -name  '*.bmc' | while read filename; do rm -f "${filename}"; done
+	rm -f $1/arch/arm/mach-aspeed/ast-bmc-scu.c
+	rm -f $1/arch/arm/mach-aspeed/include/mach/ast-bmc-scu.h
+	rm -f $1/arch/arm/mach-aspeed/include/mach/ast_bmc_platform.h
+	rm -rf $1/board/aspeed/ast2*
+	rm -rf $1/board/aspeed/ast1*
+	rm -rf $1/board/aspeed/astg*
+	rm -f $1/include/configs/ast-g*.h
+	rm -f $1/configs/ast2*_defconfig
 fi
 
+rm -rf $1/board/aspeed/ast2510
+rm -rf $1/board/aspeed/ast2150
+rm -rf $1/board/aspeed/ast2100
+rm -rf $1/board/aspeed/ast2050
 rm -f $1/arch/arm/include/asm/arch-aspeed/ast1010_platform.h
 rm -f $1/arch/arm/include/asm/arch-aspeed/ast3200_platform.h
 rm -f $1/arch/arm/cpu/arm1176/u-boot-spl.lds
 rm -f $1/arch/arm/cpu/arm1176/u-boot-spl.lds.org
-rm -rf $1/board/aspeed/ast1010
-rm -rf $1/board/aspeed/ast1400
 rm -f $1/board/aspeed/ast-g5/platform3200.S
 rm -f $1/board/aspeed/ast-g5/fast.S
 rm -f $1/board/aspeed/ast-g5/platform1520.S
 rm -f $1/board/aspeed/ast-g5/platform.uart1
 rm -f $1/drivers/gpio/ast_gpio.c
-rm -f $1/drivers/mmc/ast_sdhci.c
 rm -f $1/drivers/spi/ast_spi.c
 rm -f $1/drivers/usb/host/ehci-ast.c
 rm -f $1/include/configs/ast1010.h

8b6ccc55d198d4c2d6d98da2ed724c5ad6c640da
Ryan Chen <csf0711@gmail.com>
2017-07-21 09:30:30 +0800

    update video size

--- arch/arm/mach-aspeed/cpuinfo.c
+++ arch/arm/mach-aspeed/cpuinfo.c
@@ -56,7 +56,10 @@ int print_cpuinfo(void)
 	size = ast_scu_get_vga_memsize();
 
 	puts("VGA :    ");
-	print_size(size, " - ");
+	print_size(size, ", ");
+
+	puts("VIDEO : ");
+	print_size(CONFIG_AST_VIDEO_SIZE, " - ");
 
 	size = ast_sdmc_get_mem_size();
 	puts("Total DRAM : ");

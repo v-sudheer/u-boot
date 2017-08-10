22e2937c11b27e56d0ea20e2dbaa25a63a6b1c85
Ryan Chen <csf0711@gmail.com>
2017-08-10 11:40:54 +0800

    update

--- include/configs/ast-cam.h
+++ include/configs/ast-cam.h
@@ -110,8 +110,6 @@
 
 #define CONFIG_SYS_LOAD_ADDR	0x83000000	/* default load address */
 
-#define CONFIG_SYS_SDRAM_SIZE (192*1024*1024)		/* 192MB */
-
 #define CONFIG_BOOTARGS		"console=ttyS0,115200n8 root=/dev/ram rw init=/linuxrc"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
@@ -186,5 +184,41 @@
  * #define	 CONFIG_DRAM_UART_38400
  * 3. DRAM Type
  * #define CONFIG_DDR3_8GSTACK     DDR3 8Gbit Stack die
- */
+ -------------------------------------------------------------------------*/
+/* SPL part */
+#define CONFIG_CMD_SPL
+#define CONFIG_SPL_FRAMEWORK
+#define CONFIG_SPL_BOARD_INIT
+#define CONFIG_SPL_RAM_DEVICE
+#define CONFIG_SPL_STACK                    	0x1e7d0000
+
+/* SP location before relocation, must use scratch RAM */
+#define CONFIG_SPL_TEXT_BASE					0x1e7d0000
+/* 36kB blocks of OCM - one is on the top because of bootrom */
+#define CONFIG_SPL_MAX_SIZE						0x00010000
+
+/* BSS setup */
+#define CONFIG_SPL_LDSCRIPT     "arch/arm/mach-aspeed/u-boot-spl.lds"
+
+/* MMC support */
+#ifdef CONFIG_AST_SDHCI
+#define CONFIG_SYS_MMCSD_FS_BOOT_PARTITION     1
+#define CONFIG_SPL_FS_LOAD_PAYLOAD_NAME     "u-boot.img"
+/* Not using MMC raw mode - just for compilation purpose */
+#define CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTOR   0
+#define CONFIG_SYS_MMCSD_RAW_MODE_ARGS_SECTORS  0
+#define CONFIG_SYS_MMCSD_RAW_MODE_KERNEL_SECTOR 0
+#endif
+
+/* Address in RAM where the parameters must be copied by SPL. */
+#define CONFIG_SYS_SPL_ARGS_ADDR			0x80000000
+
+#define CONFIG_SPL_FS_LOAD_ARGS_NAME            "system.dtb"
+#define CONFIG_SPL_FS_LOAD_KERNEL_NAME          "uImage"
+
+/* for booting directly linux */
+
+/* FIT load address for RAM boot */
+#define CONFIG_SPL_LOAD_FIT_ADDRESS	0x10000000
+
 #endif	/* __CONFIG_H */

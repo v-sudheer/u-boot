44c538986fe3276bec58aca5177a2bcf37a6f15d
Ryan Chen <ryanchen.aspeed@gmail.com>
2018-04-18 17:12:24 +0800

    [update] fix set -> setenv

--- include/configs/ast-g5_evb.h
+++ include/configs/ast-g5_evb.h
@@ -21,8 +21,8 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"verify=yes\0"\
 	"update=tftp 80800000 ast2500.scr; so 80800000\0"\
-	"ramfs=set bootargs console=ttyS0,115200n8 root=/dev/ram rw init=/linuxrc\0"\
-	"squashfs=set bootargs console=ttyS0,115200n8 root=/dev/mtdblock4 rootfs=squashfs init=/linuxrc\0"\
+	"ramfs=setenv bootargs console=ttyS0,115200n8 root=/dev/ram rw init=/linuxrc\0"\
+	"squashfs=setenv bootargs console=ttyS0,115200n8 root=/dev/mtdblock4 rootfs=squashfs init=/linuxrc\0"\
 	""
 
 

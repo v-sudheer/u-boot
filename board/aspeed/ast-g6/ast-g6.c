f5f8cf2cc85a8db9ecc3128125c61c42755acffa
ryan_chen <ryan_chen@aspeedtech.com>
2018-09-06 16:59:55 +0800

    for fix mac addr

--- board/aspeed/ast-g6/ast-g6.c
+++ board/aspeed/ast-g6/ast-g6.c
@@ -23,6 +23,51 @@ int misc_init_r (void)
 #ifdef CONFIG_AST_WATCHDOG
 	wdt_start(CONFIG_AST_WATCHDOG_TIMEOUT);
 #endif
+	int update = 0, i;
+	u32 random;
+	uchar board_mac_addr[6];
+
+	for (i = 0; i < 2; i++) {
+		if (!eth_getenv_enetaddr_by_index("eth", i, board_mac_addr)) {
+			board_mac_addr[0] = 0x00;
+			board_mac_addr[1] = 0x0c;
+			board_mac_addr[2] = (random >> 20) & 0xff;
+			board_mac_addr[3] = (random >> 16) & 0xff;
+			board_mac_addr[4] = (random >> 8) & 0xff;
+			board_mac_addr[5] = (random) & 0xff + i;
+
+			if (eth_setenv_enetaddr_by_index("eth", i, board_mac_addr)) {
+				printf("Failed to set random ethernet address\n");
+			} else {
+				printf("Setting random ethernet address %pM.\n",
+					   (uchar *)&random);
+			}
+			update++;
+		}
+	}
+
+	if (update) {
+		saveenv();	
+	}
+
 	return 0;
 
 }
+
+#ifdef CONFIG_ARMV7_NONSEC
+void smp_set_core_boot_addr(unsigned long addr, int corenr)
+{
+	printf("smp_set_core_boot_addr ");
+}
+
+void smp_kick_all_cpus(void)
+{
+	printf("smp_kick_all_cpus ");
+}
+
+void smp_waitloop(unsigned previous_address)
+{
+	printf("smp_waitloop ");
+}
+#endif
+

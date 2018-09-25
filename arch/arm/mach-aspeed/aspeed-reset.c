ea132cff69c71566884b5fec27573c08304bea54
ryan_chen <ryan_chen@aspeedtech.com>
2018-09-25 10:33:37 +0800

    update for mac reset bit

--- arch/arm/mach-aspeed/aspeed-reset.c
+++ arch/arm/mach-aspeed/aspeed-reset.c
@@ -111,9 +111,9 @@ struct aspeed_reset_config {
 static struct aspeed_reset_config ast2600_reset[] = {
 	{ "MAC1", ASPEED_SCU_BASE + 0x40, BIT(11) },
 	{ "MAC2", ASPEED_SCU_BASE + 0x40, BIT(12) },
-	{ "MAC3", ASPEED_SCU_BASE + 0x50, BIT(11) },
-	{ "MAC4", ASPEED_SCU_BASE + 0x50, BIT(12) },
-	{ "MDIO", ASPEED_SCU_BASE + 0x50, BIT(13) },	
+	{ "MAC3", ASPEED_SCU_BASE + 0x50, BIT(20) },
+	{ "MAC4", ASPEED_SCU_BASE + 0x50, BIT(21) },
+	{ "MDIO", ASPEED_SCU_BASE + 0x50, BIT(3) },	
 };
 #elif defined(CONFIG_MACH_ASPEED_G4) || defined(CONFIG_MACH_ASPEED_G5)
 static struct aspeed_reset_config ast2500_reset[] = {

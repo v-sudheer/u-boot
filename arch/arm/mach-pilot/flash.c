9f657fd434019313a5c9d611c606891b26f6e601
shivahshankar <shivahshankar.shankarnarayanrao@aspeedtech.com>
2018-11-07 13:04:13 -0500

    Removed unwanted code

--- arch/arm/mach-pilot/flash.c
+++ arch/arm/mach-pilot/flash.c
@@ -22,26 +22,3 @@ int write_buff (flash_info_t * info, uchar * src, ulong addr, ulong cnt)
 void reset_cpu(ulong addr)
 {
 }
-#if 0
-int set_cpu_clk_info(void)
-{
-	return 0;
-}
-unsigned long get_uart_clk(int dev_id)
-{
-	return 0;
-}
-int zynq_slcr_get_mio_pin_status(const char *periph)
-{
-	return 0;
-}
-#endif
-#ifndef CONFIG_SPL_BUILD
-u32 spl_boot_device(void)
-{
-	return 0;
-}
-#endif
-void spl_board_init(void)
-{
-}
